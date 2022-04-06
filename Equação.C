bf215e49d19a3282ed715928fad2e93572fbf40c
Ricardo Gallozzi <92772483+RicJr06@users.noreply.github.com>
2022-04-06 20:17:36 -0300

    Create Equação.C
    Programação para resolução de equações

---
+++ Equação.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main(){
+	
+	float b, c , h, x;
+	printf("entre com o valor b: ");
+	scanf("%f", &b);
+	
+    printf("entre com o valor c: ");
+	scanf("%f", &c);
+	
+	printf("entre com o valor h: ");
+	scanf("%f", &h);
+	
+	x=b*c+h/2;
+	
+	printf("o resultado de x é= %2.0f ", x);
+    
+	
+} 
